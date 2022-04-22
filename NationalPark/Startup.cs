43bf61bf1a5ab165c995a35fb8faa0d4471e8d98
John Whitten <johnwhitten.studio@gmail.com>
2022-04-22 13:18:15 -0700

    comment out app.usehttpsredirection

--- NationalPark/Startup.cs
+++ NationalPark/Startup.cs
@@ -44,7 +44,7 @@ namespace NationalPark
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NationalPark v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
